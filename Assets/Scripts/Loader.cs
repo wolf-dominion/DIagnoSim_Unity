335f2132f3c2a9ccbed1da28ed468c835ed7775a
wolf-dominion <hannah_drury@yahoo.com>
2020-06-30 11:26:01 -0600

    Main menu not blurry anymore, main menu background image, added loading screen

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,10 +9,27 @@ public static class Loader
 
     public enum Scene {
         Structure_02,
+        LoadingScreen,
     }
 
+    private static Action onLoaderCallback;
+
     public static void Load(Scene scene) {
-        SceneManager.LoadScene(scene.ToString());
+        // set the loader callback action to load the target scene
+        onLoaderCallback = () => {
+            SceneManager.LoadScene(scene.ToString());
+        };
+        // load the loading scene
+        SceneManager.LoadScene(Scene.LoadingScreen.ToString());
+    }
+
+    public static void LoaderCallback() {
+        // triggered after the first update which lets the screen refresh
+        //execute the loader callback afction which will load the target scene 
+        if (onLoaderCallback != null) {
+            onLoaderCallback();
+            onLoaderCallback = null;
+        }
     }
 
 }
